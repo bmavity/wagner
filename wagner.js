253330ad7473a58ce968d7f6e3b20521cfc1bb99
bmavity <brian@brianmavity.com>
2013-04-28 09:05:14 -0500

    Wagner now automatically uses different parts

--- wagner.js
+++ wagner.js
@@ -1,18 +1,27 @@
 var events = require('eventemitter2')
 	, util = require('util')
 	, path = require('path')
+	, mixins = []
+
+var evt = require('./wagner.eventdelegation')
+	, formFsm = require('./wagner.form.fsm')
+	, formSubmitter = require('./wagner.form.submit')
+	, fsm = require('./wagner.fsm')
+	, ko = require('./wagner.ko')
+	, pubsub = require('./wagner.pubsub')
 
 function extend(mixin) {
 	mixin.call(Component.prototype)
 }
 
-function Component(rootPath) {
+function Component(rootPath, options) {
 	if(!(this instanceof Component)) {
-		return new Component(rootPath)
+		return new Component(rootPath, options)
 	}
 
 	var rootId = path.basename(rootPath, path.extname(rootPath))
 		, root = document.getElementById(rootId)
+	options = options || {}
 
 	Object.defineProperty(this, '_root', {
 		value: root
@@ -25,16 +34,21 @@ function Component(rootPath) {
 	events.EventEmitter2.call(this, {
 		wildcard: true
 	})
+
+	pubsub.call(this)
+	evt.call(this, options)
+	fsm.call(this)
+
+	if(root.nodeName.toLowerCase() === 'form') {
+		formSubmitter.call(this)
+		formFsm.call(this)
+	}
+
+	if(options.schema) {
+		ko.call(this, options.schema)
+	}
 }
 Component.extend = extend
 
 util.inherits(Component, events.EventEmitter2)
 module.exports = Component
-
-
-Component.evt = require('./wagner.eventDelegation')
-Component.formFsm = require('./wagner.form.fsm')
-Component.formSubmitter = require('./wagner.form.submit')
-Component.fsm = require('./wagner.fsm')
-Component.ko = require('./wagner.ko')
-Component.pubsub = require('./wagner.pubsub')
